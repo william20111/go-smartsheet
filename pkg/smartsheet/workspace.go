adcaa9f9c857d6768f68aff3fe0ce4aea41d26e0
William Fleming <wfleming@grumpysysadm.com>
2020-08-16 20:21:03 +0100

    fix(test): upper case for json

--- pkg/smartsheet/workspace.go
+++ pkg/smartsheet/workspace.go
@@ -19,62 +19,62 @@ package smartsheet
 import "time"
 
 type Workspace struct {
-	Id          int        // Workspace Id
-	AccessLevel string     // User's permissions on the workspace
-	Favorite    bool       // Returned only if the user has marked the workspace as a favorite in their "Home" tab (value = true)
-	Folders     []Folder   // Array of Folder objects
-	Name        string     // Workspace name
-	Permalink   string     // URL that represents a direct link to the workspace in Smartsheet
-	Reports     []Report   // Array of Report objects
-	Sheets      []Sheet    // Array of Sheet objects
-	Sights      []Sight    // Array of Sight objects
-	Templates   []Template // Array of Template objects
+	Id          int        `json:"id"`          // Workspace Id
+	AccessLevel string     `json:"accessLevel"` // User's permissions on the workspace
+	Favorite    bool       `json:"favorite"`    // Returned only if the user has marked the workspace as a favorite in their "Home" tab (value = true)
+	Folders     []Folder   `json:"folders"`     // Array of Folder objects
+	Name        string     `json:"name"`        // Workspace name
+	Permalink   string     `json:"permalink"`   // URL that represents a direct link to the workspace in Smartsheet
+	Reports     []Report   `json:"reports"`     // Array of Report objects
+	Sheets      []Sheet    `json:"sheets"`      // Array of Sheet objects
+	Sights      []Sight    `json:"sights"`      // Array of Sight objects
+	Templates   []Template `json:"templates"`   // Array of Template objects
 }
 
 type Folder struct {
-	Id        int        // Folder Id
-	Favorite  bool       // Returned only if the user has marked the folder as a favorite in their "Home" tab (value = true)
-	Folders   []Folder   // Array of Folder objects
-	Name      string     // Folder name
-	Permalink string     // URL that represents a direct link to the folder in Smartsheet
-	Reports   []Report   // Array of Report objects
-	Sheets    []Sheet    // Array of Sheet objects
-	Sights    []Sight    // Array of Sight objects
-	Templates []Template // Array of Template objects
+	Id        int        `json:"id"`        // Folder Id
+	Favorite  bool       `json:"favorite"`  // Returned only if the user has marked the folder as a favorite in their "Home" tab (value = true)
+	Folders   []Folder   `json:"folders"`   // Array of Folder objects
+	Name      string     `json:"name"`      // Folder name
+	Permalink string     `json:"permalink"` // URL that represents a direct link to the folder in Smartsheet
+	Reports   []Report   `json:"reports"`   // Array of Report objects
+	Sheets    []Sheet    `json:"sheets"`    // Array of Sheet objects
+	Sights    []Sight    `json:"sights"`    // Array of Sight objects
+	Templates []Template `json:"templates"` // Array of Template objects
 }
 
 type Report struct {
-	Scope        Scope   // A report's scope can be defined as the sheet ids and workspace ids that make up the report.
-	SourceSheets []Sheet // Array of Sheet objects (without rows), representing the sheets that rows in the report originated from. Only included in the Get Report response if the include parameter specifies sourceSheets.
+	Scope        Scope   `json:"scope"`        // A report's scope can be defined as the sheet ids and workspace ids that make up the report.
+	SourceSheets []Sheet `json:"sourceSheets"` // Array of Sheet objects (without rows), representing the sheets that rows in the report originated from. Only included in the Get Report response if the include parameter specifies sourceSheets.
 }
 
 type Sight struct {
-	Id              int       // Sight Id
-	AccessLevel     string    // User's permissions on the Sight
-	BackgroundColor string    // The hex color, for instance #E6F5FE
-	ColumnCount     int       //	Number of columns that the Sight contains
-	CreatedAt       time.Time //	Time of creation
-	Favorite        bool      //	Indicates whether the user has marked the Sight as a favorite
-	ModifiedAt      time.Time //	Time of last modification
-	Name            string    //	Sight name
-	Permalink       string    //	URL that represents a direct link to the Sight in Smartsheet
-	Source          Source    //	A Source object indicating the Sight (aka dashboard) from which this Sight was created, if any
-	Widgets         []Widget  //	Array of Widget objects
-	Workspace       Workspace //	A Workspace object, limited to only 2 attributes:
+	Id              int       `json:"id"`              // Sight Id
+	AccessLevel     string    `json:"accessLevel"`     // User's permissions on the Sight
+	BackgroundColor string    `json:"backgroundColor"` // The hex color, for instance #E6F5FE
+	ColumnCount     int       `json:"columnCount"`     //	Number of columns that the Sight contains
+	CreatedAt       time.Time `json:"createdAt"`       //	Time of creation
+	Favorite        bool      `json:"favorite"`        //	Indicates whether the user has marked the Sight as a favorite
+	ModifiedAt      time.Time `json:"modifiedAt"`      //	Time of last modification
+	Name            string    `json:"name"`            //	Sight name
+	Permalink       string    `json:"permalink"`       //	URL that represents a direct link to the Sight in Smartsheet
+	Source          Source    `json:"source"`          //	A Source object indicating the Sight (aka dashboard) from which this Sight was created, if any
+	Widgets         []Widget  `json:"widgets"`         //	Array of Widget objects
+	Workspace       Workspace `json:"workspace"`       //	A Workspace object, limited to only 2 attributes:
 }
 
 type Widget struct {
-	Id            int         // 	Widget Id
-	Type          string      // Type of widget. See table below to see how UI widget names map to type.
-	Contents      interface{} // object 	Data that specifies the contents of the widget. NOTE: The type of WidgetContent object (and attributes within) depends on the value of widget.type.
-	Height        int         // 	Number of rows that the widget occupies on the Sight
-	ShowTitle     bool        //	True indicates that the client should display the widget title. NOTE: This is independent of the title string which may be null or empty.
-	ShowTitleIcon bool        //	True indicates that the client should display the sheet icon in the widget title
-	Title         string      // Title of the widget
-	TitleFormat   string      // FormatDescriptor
-	Version       int         // Widget version int //
-	ViewMode      int         //	1 indicates content is centered. 2 indicates content is left aligned. Must use a query parameter of level=2 to see this information.
-	Width         int         // 	Number of columns that the widget occupies on the Sight
-	XPosition     int         // 	X-coordinate of widget's position on the Sight
-	YPosition     int         // 	Y-coordinate of widget's position on the Sight
+	Id            int         `json:"id"`            // 	Widget Id
+	Type          string      `json:"type"`          // Type of widget. See table below to see how UI widget names map to type.
+	Contents      interface{} `json:"contents"`      // object 	Data that specifies the contents of the widget. NOTE: The type of WidgetContent object (and attributes within) depends on the value of widget.type.
+	Height        int         `json:"height"`        // 	Number of rows that the widget occupies on the Sight
+	ShowTitle     bool        `json:"showTitle"`     //	True indicates that the client should display the widget title. NOTE: This is independent of the title string which may be null or empty.
+	ShowTitleIcon bool        `json:"showTitleIcon"` //	True indicates that the client should display the sheet icon in the widget title
+	Title         string      `json:"title"`         // Title of the widget
+	TitleFormat   string      `json:"titleFormat"`   // FormatDescriptor
+	Version       int         `json:"version"`       // Widget version int //
+	ViewMode      int         `json:"viewMode"`      //	1 indicates content is centered. 2 indicates content is left aligned. Must use a query parameter of level=2 to see this information.
+	Width         int         `json:"width"`         // 	Number of columns that the widget occupies on the Sight
+	XPosition     int         `json:"xPosition"`     // 	X-coordinate of widget's position on the Sight
+	YPosition     int         `json:"yPosition"`     // 	Y-coordinate of widget's position on the Sight
 }
